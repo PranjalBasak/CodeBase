193e8be01265f9451da6fd4c69ac7442ba62dd5f
PranjalBasak <66166653+PranjalBasak@users.noreply.github.com>
2022-11-26 14:21:00 +0600

    Create code1.cpp

---
+++ code1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define nl "\n"
+const double pi = 3.14159265358979323846;
+
+using namespace std;
+
+int main(){
+	int a = 5, b = 6;
+	double x = sqrt(pow((a + b), 2) / 2);
+	cout << x;
+
+
+	return 0;
+}
